9afd7977462e475cdd502381accd2903d8c08d17
silvyc <smchain@uninorte.edu.co>
2022-04-24 22:24:20 -0500

    udpdate logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import {useState} from 'react';
-
 function App() {
   const [cont,setCont] = useState(0);
   const incNum = ( ) => {
@@ -12,22 +10,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="hola-mundo">Hola mundo</h1>
-        <Text>{cont}</Text>
-        <button onClick={incNum}>Click</button>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/img/icono-covid.png" className="App-logo" alt="logo" align='left'/>
+        <Text className = "Titulo-Header">COVID NEWS</Text>
       </header>
     </div>
   );
