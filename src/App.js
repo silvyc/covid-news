affe0f3abec07e84a40ca14ec0aff7009ba89670
silvyc <smchain@uninorte.edu.co>
2022-04-24 22:49:01 -0500

    favicon y titulo cambiado

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import { Button } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import {useState} from 'react';
+
 function App() {
   const [cont,setCont] = useState(0);
   const incNum = ( ) => {
     setCont((prevState)=> prevState + 1);
   }
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src="/img/icono-covid.png" className="App-logo" alt="logo" align='left'/>
-        <Text className = "Titulo-Header">COVID NEWS</Text>
+        <text className = "Titulo-Header" align='left'>COVID NEWS</text>
       </header>
     </div>
   );
