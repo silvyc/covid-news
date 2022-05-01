38aea9c99fe69911c3ffffcf35747a0c2c3dbfaa
silvyc <smchain@uninorte.edu.co>
2022-05-01 18:13:47 -0500

    Update about us

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,10 +1,55 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, HStack, Image, Stack, Text, VStack } from "@chakra-ui/react";
 
 export default function About() {
   return (
     <Box>
-      <Text>Aquí está el micropene de guarito</Text>
-      <Image src="https://6lli539m39y3hpkelqsm3c2fg-wpengine.netdna-ssl.com/wp-content/uploads/2013/11/image2.jpg" />
+      <Text fontSize='4xl' align={'center'}>About us</Text>
+      <Stack
+        direction={'column'}
+      >
+        <HStack
+          spacing='25px'
+          alignItems='center'
+        >
+          <Image  
+            borderRadius='full'
+            boxSize='150px'
+            alt="Silvana Chain" 
+            src='/img/SilvanaChain.jpg' />
+            <VStack alignItems={'flex-start'} justifyContent={'flex-start'}>
+            <Text fontSize='xl'>
+              Silvana Maria Alexandra Chain Marulanda 
+            </Text>
+            <Text align={"left"}>
+              smchain@uninorte.edu.co
+            </Text>
+            <Text align={"left"}>
+              Codigo Estudiantil: 200143952
+            </Text>
+            </VStack>
+        </HStack>
+        <HStack
+          spacing='25px'
+          alignItems='center'
+        >
+          <Image 
+            borderRadius='full'
+            boxSize='150px'
+            alt="Juan Padilla" 
+            src='/img/JuanPadilla.jpg'/>
+            <VStack alignItems={'flex-start'} justifyContent={'flex-start'}>
+            <Text fontSize='xl'>
+              Juan David Padilla Paez
+            </Text>
+            <Text align={"left"}>
+              paezdj@uninorte.edu.co
+            </Text>
+            <Text align={"left"}>
+              Codigo Estudiantil: 200145173
+            </Text>
+            </VStack>
+        </HStack>
+      </Stack>
     </Box>
   );
 }
