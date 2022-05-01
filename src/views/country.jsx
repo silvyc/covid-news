a6f99cd4b0034313ffd75f3242a7d5c12b9b9ec5
Ayano <s.correa.wilches@gmail.com>
2022-05-02 06:47:06 +0700

    Updated home spacings

--- src/views/country.jsx
+++ src/views/country.jsx
@@ -10,7 +10,11 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { getCovidRegionHundred, getCovidRegionInfo, getCovidRegionNew } from "../api/covidcasescountry";
+import {
+  getCovidRegionHundred,
+  getCovidRegionInfo,
+  getCovidRegionNew,
+} from "../api/covidcasescountry";
 
 ChartJS.register(
   CategoryScale,
@@ -33,6 +37,7 @@ export const options1 = {
     },
   },
 };
+
 export const options2 = {
   responsive: true,
   plugins: {
@@ -58,7 +63,6 @@ export const options3 = {
   },
 };
 
-
 export default function Daily() {
   const [regionInfo, setRegionInfo] = useState([]);
   const [regionNewData, setRegionNewData] = useState([]);
@@ -76,28 +80,28 @@ export default function Daily() {
         {
           label: "Deaths",
           data: regionInfo.map((country) => country.region_deaths),
-          backgroundColor: 'rgba(54, 99, 255, 0.4)',
+          backgroundColor: "rgba(54, 99, 255, 0.4)",
         },
         {
           label: "Recovered",
           data: regionInfo.map((country) => country.region_recovered),
-          backgroundColor: 'rgba(54, 99, 255, 0.6)',
+          backgroundColor: "rgba(54, 99, 255, 0.6)",
         },
         {
           label: "Confirmed",
           data: regionInfo.map((country) => country.region_confirmed),
-          backgroundColor: 'rgba(54, 99, 255, 0.8)',
+          backgroundColor: "rgba(54, 99, 255, 0.8)",
         },
         {
           label: "Active",
           data: regionInfo.map((country) => country.region_active),
-          backgroundColor: 'rgba(54, 99, 255, 1)',
+          backgroundColor: "rgba(54, 99, 255, 1)",
         },
       ],
     }),
     [regionInfo, labels]
   );
-  
+
   const data2 = useMemo(
     () => ({
       labels,
@@ -105,41 +109,47 @@ export default function Daily() {
         {
           label: "New Cases",
           data: regionNewData.map((newcases) => newcases.new_cases),
-          backgroundColor: 'rgba(54, 99, 255, 0.4)',
+          backgroundColor: "rgba(54, 99, 255, 0.4)",
         },
         {
           label: "New Deaths",
           data: regionNewData.map((newcases) => newcases.new_deaths),
-          backgroundColor: 'rgba(54, 99, 255, 0.6)',
+          backgroundColor: "rgba(54, 99, 255, 0.6)",
         },
         {
           label: "New Recovered",
           data: regionNewData.map((newcases) => newcases.new_recovered),
-          backgroundColor: 'rgba(54, 99, 255, 0.8)',
+          backgroundColor: "rgba(54, 99, 255, 0.8)",
         },
       ],
     }),
     [regionNewData, labels]
   );
-  
+
   const data3 = useMemo(
     () => ({
       labels,
       datasets: [
         {
           label: "Deaths per 100 Cases",
-          data: regionHundredCases.map((hundredcases) => hundredcases.deaths_100cases),
-          backgroundColor: 'rgba(54, 99, 255, 0.4)',
+          data: regionHundredCases.map(
+            (hundredcases) => hundredcases.deaths_100cases
+          ),
+          backgroundColor: "rgba(54, 99, 255, 0.4)",
         },
         {
           label: "Recovered per 100 Cases",
-          data: regionHundredCases.map((hundredcases) => hundredcases.recovered_100cases),
-          backgroundColor: 'rgba(54, 99, 255, 0.6)',
+          data: regionHundredCases.map(
+            (hundredcases) => hundredcases.recovered_100cases
+          ),
+          backgroundColor: "rgba(54, 99, 255, 0.6)",
         },
         {
           label: "Deaths per 100 Recovered",
-          data: regionHundredCases.map((hundredcases) => hundredcases.deaths_100recovered),
-          backgroundColor: 'rgba(54, 99, 255, 0.8)',
+          data: regionHundredCases.map(
+            (hundredcases) => hundredcases.deaths_100recovered
+          ),
+          backgroundColor: "rgba(54, 99, 255, 0.8)",
         },
       ],
     }),
@@ -159,14 +169,12 @@ export default function Daily() {
         sm: "80vw",
         md: "50vw",
       }}
-    > 
+    >
       <VStack spacing={20}>
-      <Bar data={data1} options={options1} />
-      <Bar data={data2} options={options2} />
-      <Bar data={data3} options={options3} />
+        <Bar data={data1} options={options1} />
+        <Bar data={data2} options={options2} />
+        <Bar data={data3} options={options3} />
       </VStack>
-      
     </Box>
-    
   );
 }
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,31 +1,60 @@
-import { AspectRatio, Box, Text} from "@chakra-ui/react";
+import { AspectRatio, Box, StackDivider, Text, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box>
-      <Text fontSize='4xl'>What is COVID-19?</Text>
-      <Text>
-        Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
-        Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age. 
-        The best way to prevent and slow down transmission is to be well informed about the disease and how the virus spreads. Protect yourself and others from infection by staying at least 1 metre apart from others, wearing a properly fitted mask, and washing your hands or using an alcohol-based rub frequently. Get vaccinated when it’s your turn and follow local guidance.
-        </Text>
-        <Text> - </Text>
-        <Text>
-        The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell.
-      </Text>  
-      <Text> - </Text>
-      <Text fontSize='4xl'>COVID-19 Summary video</Text>
-      <AspectRatio maxW='660px' ratio={2}>
-        <iframe 
-        width="650" 
-        height="300" 
-        src="https://www.youtube.com/embed/jWMUBouaqb0" 
-        title="COVID19 SUMMARY" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-        </iframe>
-      </AspectRatio> 
+    <Box maxWidth="4xl">
+      <VStack
+        divider={<StackDivider borderColor="gray.200" />}
+        spacing={4}
+        align="stretch"
+      >
+        <Box>
+          <Text fontSize="3xl" fontWeight="bold" mb={2}>
+            What is COVID-19?
+          </Text>
+          <Text textAlign="justify" mb={3}>
+            Coronavirus disease (COVID-19) is an infectious disease caused by
+            the SARS-CoV-2 virus. Most people infected with the virus will
+            experience mild to moderate respiratory illness and recover without
+            requiring special treatment. However, some will become seriously ill
+            and require medical attention. Older people and those with
+            underlying medical conditions like cardiovascular disease, diabetes,
+            chronic respiratory disease, or cancer are more likely to develop
+            serious illness. Anyone can get sick with COVID-19 and become
+            seriously ill or die at any age. The best way to prevent and slow
+            down transmission is to be well informed about the disease and how
+            the virus spreads. Protect yourself and others from infection by
+            staying at least 1 metre apart from others, wearing a properly
+            fitted mask, and washing your hands or using an alcohol-based rub
+            frequently. Get vaccinated when it’s your turn and follow local
+            guidance.
+          </Text>
+          <Text textAlign="justify">
+            The virus can spread from an infected person’s mouth or nose in
+            small liquid particles when they cough, sneeze, speak, sing or
+            breathe. These particles range from larger respiratory droplets to
+            smaller aerosols. It is important to practice respiratory etiquette,
+            for example by coughing into a flexed elbow, and to stay home and
+            self-isolate until you recover if you feel unwell.
+          </Text>
+        </Box>
+        <Box>
+          <Text fontSize="3xl" fontWeight="bold" mb={2}>
+            COVID-19 Summary video
+          </Text>
+          <AspectRatio maxW="660px" ratio={2}>
+            <iframe
+              width="650"
+              height="300"
+              src="https://www.youtube.com/embed/jWMUBouaqb0"
+              title="COVID19 SUMMARY"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </AspectRatio>
+        </Box>
+      </VStack>
     </Box>
   );
 }
