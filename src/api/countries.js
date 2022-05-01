73d3bf2dbdaec8d976db758b3d5b07e4538834ca
Ayano <s.correa.wilches@gmail.com>
2022-05-01 17:19:25 +0700

    Added request error catching

--- src/api/countries.js
+++ src/api/countries.js
@@ -1,9 +1,13 @@
 export const getCountries = async () => {
-  const request = await fetch("http://localhost:8000/countries/", {
-    method: "GET",
-    headers: new Headers({ "Content-type": "application/json" }),
-    mode: "cors",
-  });
-  const data = await request.json();
-  return data;
+  try {
+    const request = await fetch("http://localhost:8000/countries/", {
+      method: "GET",
+      headers: new Headers({ "Content-type": "application/json" }),
+      mode: "cors",
+    });
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
 };
